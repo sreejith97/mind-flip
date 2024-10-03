a8b9e1e5d7c71107f33d7e485f98c1393baa3925
Sreejith <sreejithsp711@gmail.com>
2024-10-03 15:53:13 +0530

    feat:game front page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,23 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hey this is our first page</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-br from-green-300 to-blue-300">
+      <h1 className="text-4xl font-bold mb-6 text-white">
+        Welcome to the Game Hub
+      </h1>
+      <div className="flex flex-col md:flex-row gap-6">
+        <Link href={"card"}>
+          <p className="px-6 py-3 bg-green-600 text-white rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-green-500">
+            Card Game
+          </p>
+        </Link>
+        <Link href={"spinner"}>
+          <p className="px-6 py-3 bg-green-600 text-white rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-green-500">
+            Spinner Game
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
 }
